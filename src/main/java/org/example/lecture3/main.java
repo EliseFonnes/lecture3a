c5378a3b4fa86eea9ce065d51faaf5962087c688
EliseFonnes <fonneselise@gmail.com>
2021-09-06 18:08:03 +0200

    Initial commit

---
+++ src/main/java/org/example/lecture3/main.java
@@ -0,0 +1,12 @@
+package org.example.lecture3;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+    public String hello()  {
+        return "hello";
+
+    }
+}
