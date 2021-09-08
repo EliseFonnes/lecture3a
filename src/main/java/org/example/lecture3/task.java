c877a38d5792ba7293884e3eeb7058bbb40fc06a
bandonice <bastian@rausand.no>
2021-09-08 15:44:36 +0200

    Endret x til X i task.java

--- src/main/java/org/example/lecture3/task.java
+++ src/main/java/org/example/lecture3/task.java
@@ -7,7 +7,7 @@ public class task {
     public static String intToRoman(int num) {
         String[] thousands = new String[]{"", "M", "MM", "MMM"};
         String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-        String[] tens = {"", "x", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
         String[] units = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
         return thousands[num / 1000] +
             hundreds[(num % 1000) /100] +
--- src/test/java/org/example/lecture3/lecture3/test.java
+++ src/test/java/org/example/lecture3/lecture3/test.java
@@ -1,4 +1,18 @@
 package org.example.lecture3.lecture3;
 
+import org.example.lecture3.task;
+import org.junit.jupiter.api.Test;
+
+import static  org.junit.jupiter.api.Assertions.assertEquals;
+
 public class test {
-}
+
+    @Test
+
+    void shouldConvertTo1() {
+        assertEquals("I", task.intToRoman(1));
+    }
+
+         }
+
+
