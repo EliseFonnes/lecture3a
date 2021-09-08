8f55919c7ab68e8d2ae166a885106dc9d46eaed7
gardemanuelsen <garema@student.kristiania.no>
2021-09-08 16:00:15 +0200

    fixing .xml file again and making dummyTest

--- src/test/java/org/example/lecture3/lecture3/test2.java
+++ src/test/java/org/example/lecture3/lecture3/test2.java
@@ -1,5 +1,14 @@
 package org.example.lecture3.lecture3;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class test2 {
+
+    @Test
+    void dummyTest() {
+        assertEquals(42, 6*7);
+    }
 
 }
