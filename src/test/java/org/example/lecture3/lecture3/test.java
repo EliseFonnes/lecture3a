741266ed73694be3798193c9ad1d568e5720fde3
gardemanuelsen <garema@student.kristiania.no>
2021-09-08 15:40:04 +0200

    fixing .xml file

--- src/test/java/org/example/lecture3/lecture3/test.java
+++ src/test/java/org/example/lecture3/lecture3/test.java
@@ -1,5 +1,14 @@
 package org.example.lecture3.lecture3;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class test {
+
+    @Test
+    void dummyTest() {
+        assertEquals(42, 6*7);
+    }
 
 }
